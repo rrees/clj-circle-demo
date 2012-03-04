962d1a778261b36bb87a8a20646c3aea36ee6ed7
Robert Rees <rrees@roberts-macbook.local>
2012-03-04 22:42:07 +0000

    Adding processing

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clj-circle-demo "1.0"
   :description "Interactive demo"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+	[processing-core/processing.core "0.1.0"]])
