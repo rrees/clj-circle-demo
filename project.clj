dac0fac612f187b4d4c915b293d98690a4e4b891
Robert Rees <robert@wazoku.com>
2012-03-10 18:56:50 +0000

    Got something working at least

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-circle-demo "1.0"
   :description "Interactive demo"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-	[processing-core/processing.core "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+  	[org.clojars.automata/rosado.processing "1.1.0"]])
--- src/clj_circle_demo/core.clj
+++ src/clj_circle_demo/core.clj
@@ -1,1 +1,43 @@
-(ns clj-circle-demo.core)
+(ns clj-circle-demo.core
+  (:use [rosado.processing]
+        [rosado.processing.applet])
+  (:gen-class))
+
+(def screen-size 800)
+
+(def radar-range 400)
+
+(def direction (atom 0))
+
+(defn radar-centre []
+	[(/ screen-size 2) (/ screen-size 2)])
+
+(defn radar-end [direction start-x start-y]
+	(let [ sine (sin direction)
+		cosine (cos direction)]
+	[(+ (* sine radar-range) start-x) (+ (* cosine radar-range) start-y)]))
+
+(defn draw []
+	(let [[centre-x centre-y] (radar-centre)
+			[end-x end-y] (radar-end @direction centre-x centre-y)]
+		(stroke 125)
+		(swap! direction inc)
+		(line centre-x centre-y end-x end-y)
+		)
+)
+
+(defn setup []
+   (smooth)
+  (no-stroke)
+  (fill 226)
+  (framerate 20))
+
+;; Now we just need to define an applet:
+
+(defapplet radar
+	:title "Radar interactive demo"
+	:setup setup
+	:draw draw
+	:size [screen-size screen-size])
+
+(run radar)
