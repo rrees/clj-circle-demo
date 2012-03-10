05b4c7cabaa78b210d7252966e23ec92704fb47a
Robert Rees <robert@wazoku.com>
2012-03-10 19:40:45 +0000

    Introducing atoms, making a note of the successful interactive command

--- src/clj_circle_demo/core.clj
+++ src/clj_circle_demo/core.clj
@@ -5,9 +5,13 @@
 
 (def screen-size 800)
 
-(def radar-range 400)
+(def radar-range (atom 400))
+
+(def radar-colour (atom 125))
 
 (def direction (atom 0))
+
+(def sweep-increment (atom 0.1))
 
 (defn radar-centre []
 	[(/ screen-size 2) (/ screen-size 2)])
@@ -15,12 +19,17 @@
 (defn radar-end [direction start-x start-y]
 	(let [ sine (sin direction)
 		cosine (cos direction)]
-	[(+ (* sine radar-range) start-x) (+ (* cosine radar-range) start-y)]))
+	[(+ (* sine @radar-range) start-x) (+ (* cosine @radar-range) start-y)]))
+
+(defn increment-sweep [current-direction]
+	(if (> current-direction (* Math/PI 2))
+		0
+		(+ current-direction @sweep-increment)))
 
 (defn draw []
 	(let [[centre-x centre-y] (radar-centre)
 			[end-x end-y] (radar-end @direction centre-x centre-y)]
-		(stroke 125)
+		(stroke @radar-colour)
 		(swap! direction inc)
 		(line centre-x centre-y end-x end-y)
 		)
@@ -40,4 +49,9 @@
 	:draw draw
 	:size [screen-size screen-size])
 
-(run radar)
+
+(defn start-demo []
+	(run radar))
+
+(defn stop-demo []
+	(stop radar))
